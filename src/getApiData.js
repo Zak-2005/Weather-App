cc05d9c5da09ed1e16e93b4933c8a290450cb03b
Zak-2005 <zak.mob2005@gmail.com>
2023-06-06 17:42:39 -0500

    add getWeatherData function to get data from weather api

---
+++ src/getApiData.js
@@ -0,0 +1,8 @@
+export async function getWeatherData() {
+  let location = document.querySelector("input").value;
+  let response = await fetch(
+    `https://api.weatherapi.com/v1/current.json?key=2e3b564a59934e5987d160916230306&q=${location}`,
+    { mode: "cors" }
+  );
+   let weatherData = await response.json();
+}
